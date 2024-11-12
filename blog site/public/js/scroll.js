2b4ead03dedb7d293fae811355a51b89c9a3d46d
MuratArpacioglu <arpacioglumurat@icloud.com>
2024-11-12 23:07:21 +0300

    Add files via upload

---
+++ blog site/public/js/scroll.js
@@ -0,0 +1,18 @@
+const targets = document.querySelectorAll('.target')
+
+const options = {
+    threshold: 0.3
+}
+
+const callBack =(entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+    })
+}
+
+const observer = new IntersectionObserver(callBack, options);
+
+targets.forEach((target) => {
+    observer.observe(target)
+})
+
